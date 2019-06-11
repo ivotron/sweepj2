074b898744db967a4b31f41f580be0e7898dbc69
arshul <arshulmohd123@gmail.com>
2019-06-12 01:45:22 +0530

    other stuff

--- output/output-(20, 0.8, 3).sh
+++
@@ -1,1 +0,0 @@
-./fasttext supervised -input cooking.train -output model_cooking -wordNgrams 3 -lr 0.8 -epoch 20
--- output/output-(30, 0.3, 2).sh
+++
@@ -1,1 +0,0 @@
-./fasttext supervised -input cooking.train -output model_cooking -wordNgrams 2 -lr 0.3 -epoch 30
