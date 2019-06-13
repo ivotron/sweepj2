6e550f4ac2d6166d9e1522942dfe8304eead3cff
arshul <arshulmohd123@gmail.com>
2019-06-13 11:05:25 +0530

    removed extra lines, fixed setup.py updated travis

--- setup.py
+++ setup.py
@@ -1,26 +1,17 @@
 from setuptools import setup
 
-# version = {}
-# with open('popper/__init__.py') as f:
-#     exec(f.read(), version)
-
 setup(
     name='sweepj2',
     version='1.0',
     author='The Popper Development Team',
     author_email='ivo@cs.ucsc.edu',
-    url='http://falsifiable.us',
+    url='https://github.com/ivotron/sweepj2',
     description='Utilities for running parameter sweeps by providing a Jinja2 template and a parameter space (in YAML).',
-    packages=['sweepj2'],
-    include_package_data=True,
+    scripts=['bin/sweepj2'],
     install_requires=[
         'Jinja2',
         'pyyaml'
     ],
-    entry_points='''
-        [console_scripts]
-        popper=popper.cli:cli
-    ''',
     keywords='parameter sweep, grid search',
     license='MIT',
 )
